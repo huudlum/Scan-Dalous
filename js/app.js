80e5b5fe1ac979d30fb97bff3c7eed61bda9b457
huudlum <103367214+huudlum@users.noreply.github.com>
2024-08-29 21:40:45 +0100

    V2.1
    Added link to Github and code comments.

--- js/app.js
+++ js/app.js
@@ -5,6 +5,11 @@ let qrDownload = document.getElementById("qrDownload");
 let refreshBtn = document.getElementById("refreshBtn")
 let downloadBtn = document.getElementById("downloadBtn");
 
+/*function to generate the QR code
+checks for the length of the text entered and calls the api to generate the qr code - adding the entered text
+to the end of the URL. Also enables download link and then enables the display of the QR code and the 2 other buttons.
+Otherwise it adds the error class.
+*/
 function generateQR() {
     if (qrText.value.trim().length > 0) {
         qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
