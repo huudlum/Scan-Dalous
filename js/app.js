bdacf737f6ba441af0baa6a3dcaf68735261e5a1
huudlum <103367214+huudlum@users.noreply.github.com>
2024-08-29 21:12:49 +0100

    V1.2
    Added in footer
    Added in download button and corresponding javascript to hide and show buttons on click and to enable downloading of QR code

--- js/app.js
+++ js/app.js
@@ -1,12 +1,24 @@
 let qrBox = document.getElementById("qrBox");
 let qrImg = document.getElementById("qrImg");
 let qrText = document.getElementById("qrText");
+let qrDownload = document.getElementById("qrDownload");
+let refreshBtn = document.getElementById("refreshBtn")
+let downloadBtn = document.getElementById("downloadBtn");
+
 function generateQR() {
-    if (qrText.value.trim().length > 0){
+    if (qrText.value.trim().length > 0) {
         qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
             + qrText.value;
+
+        qrImg.onload = function downloadQR() {
+            qrDownload.href = qrImg.src;
+        }
+
         qrBox.classList.add("show-img");
+        refreshBtn.classList.add("show-btn");
+        downloadBtn.classList.add("show-btn");
     }
+
     else{
         qrText.classList.add("error");
         setTimeout(()=>{
